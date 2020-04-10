a097445ae600b432de11ea67b6e48b8177be1ceb
Mandar Palekar <mpalekar@tnsi.com>
2020-04-10 14:19:49 +0930

    Initial commit of rest client

---
+++ src/test/java/com/mandar/brewery/breweryclientapplication/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandar.brewery.breweryclientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
